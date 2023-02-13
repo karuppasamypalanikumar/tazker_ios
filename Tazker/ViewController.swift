d078704187a6bb30a864ccbed05e296934a1605e
Karuppasamy Palanikumar <karuppasamypalanikumar@gmail.com>
2023-02-13 09:46:21 +0530

    Initial Commit

---
+++ Tazker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tazker
+//
+//  Created by Karuppasamy Palanikumar on 13/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
