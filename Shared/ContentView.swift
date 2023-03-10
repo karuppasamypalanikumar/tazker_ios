b344b49f896fe8274d29ebc5bcb6ff733be0d9cf
Karuppasamy Palanikumar <karuppasamypalanikumar@gmail.com>
2023-03-10 14:30:11 +0530

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Karuppasamy Palanikumar on 10/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/TazkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TazkerApp.swift
+//  Shared
+//
+//  Created by Karuppasamy Palanikumar on 10/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TazkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
