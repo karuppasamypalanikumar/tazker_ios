0435a4aa68fa32b8353eceaff045003940353d7f
Karuppasamy Palanikumar <karuppasamypalanikumar@gmail.com>
2023-03-12 20:00:41 +0530

    Initial Page Design Started -> Karuppasamy

---
+++ Shared/Helpers/Colors.swift
@@ -0,0 +1,15 @@
+//
+//  Colors.swift
+//  Tazker (iOS)
+//
+//  Created by Karuppasamy Personal on 12/03/23.
+//
+
+import Foundation
+import SwiftUI
+
+typealias Colors = String
+
+extension Colors {
+    static var PrimaryColor: Color  { get { return Color(red: 0.58, green: 0.011, blue: 0.988) } }
+}
